7740b75ca10791b713293360894885d1a4226f74
Savio Concessio <savio.concessio@outlook.com>
2024-09-18 00:19:10 +1200

    Reset parsing stance: now parsing bytext chunk dealing with braces seprately, started with states - R,A,I and S in development

--- project.cpp
+++ project.cpp
@@ -1,20 +1,19 @@
-#include <iostream>
-#include <string>
 #include <map>
 #include <functional>
-#include <fstream>
+#include <stack>
 #include "labfunctions.h"
+#include "out.h"
 using namespace std;
 
 struct state {
     string name;
     map<string, state*> transitions; 
-	bool accepting = false;
+	bool accepting = false, init = false;
 	
     state(string n) : name(n) {}
-
+	state(){}
     void add(const string& k, state* v) {
-        transitions.emplace(k,v); 
+        transitions[k] = v; 
     }
 
     state(const state&) = delete;
@@ -24,34 +23,102 @@ struct state {
 	}
 };
 
+vector<string> code;
+map<string, state>all_states;
+stack<string> braces;//checks if braces are closed 
+state editing;
+int i = 0;
+string input;
+enum parse_state{
+	R, A, S, I, T, It, D, N
+};
+parse_state cur_state = R;
 
+class ParseError : public std::exception {
+private:
+    std::string message; 
 
-string read_spec_file(string filename){
-  cout<<"Reading specifications file "<<filename<<endl;
-  string out;
-  ifstream inputFile(filename);
-  string line;
-  out = "";
-  string n1;
-  while(getline(inputFile,line)) { 
-     //std::cout<<"from file="<<line<<std::endl;
-     // remove all new line characters;
-     line.erase( remove(line. begin(), line.end(),'\r'), line.end() );
-     line.erase( remove(line. begin(), line.end(),'\n'), line.end() );
-     line.erase( remove(line. begin(), line.end(),' '), line.end() );
-     out.append(line);
-  }
-  //cout<<" input="<< input<<endl;
-  return out;
+public:
+    ParseError(const std::string& cause) : message(cause) {}
+
+    const char* what() const noexcept override {
+        return message.c_str();  // Convert the string to a C-style string
+    }
+};
+
+void parse(string str){
+    switch(cur_state){
+        case R: {
+            if(str.length() != 1) throw ParseError("Illegal amount of args @ R: " + str);
+            char c = str[0];
+            if(c == 's') cur_state = S;
+            else if(c == 'a') cur_state = A;
+            else if(c == 'i') cur_state = I;
+            else throw ParseError("Bad char passed to case R: " + str);
+            if(input[i++] != '{') throw ParseError("Missing { @ R:");
+            break;
+        }
+        case A: {
+            auto it = all_states.find(str);  // Use str instead of "5"
+            if (it != all_states.end()) {
+                it->second.accepting = true;
+            } else {
+                state to_add(str);  // Use parentheses instead of equals
+                to_add.accepting = true;  // Set the accepting state
+                all_states[str] = to_add;             
+             }
+              if(input[i++] != '}') throw ParseError("Missing { @ R:");
+              cur_state = R;
+            break;
+        }
+        case S: {
+			 if(str.length() != 1) throw ParseError("Illegal amount of args @ R: " + str);
+			 char c = str[0];
+			 if(c == 't') cur_state = S;
+			 else if(c == 'n') cur_state = A;
+             else throw ParseError("Bad char passed to case R: " + str);
+			 if(input[i++] != '{') throw ParseError("Missing { @ R:");
+             break;
+        }
+        case I: {
+            auto it = all_states.find(str);
+            if (it != all_states.end()) {
+                it->second.init = true;
+            } else {
+                state to_add(str);
+                to_add.init = true;
+                all_states[str] = to_add;             
+            }
+            if(input[i++] != '}') throw ParseError("Missing { @ R:");
+            cur_state = R;
+            break;
+        }
+        case T:
+        case It:
+        case D:
+        case N:
+            break;
+    }
 }
 
-void parse_string(string input){
-	
-
+void parse_string(){
+	parse(string(1,input[i++]));
+	while(i < input.length()){
+		string cur = "";
+		//char to_process = input[i++];
+		//to_process = input[i++];
+		while(input[i]!= '{'||input[i] != '}'){
+			cur.push_back(input[i++]);
+		}
+		//if(to_process != '}')throw ParseError("Expected {, found :" + to_process);
+		parse(cur);
+	}
 }
 
 
 int main(){
-	string fromFile = read_spec_file(getString());
+	string fromFile = ReadSpecFile(getString());
+	cout<<fromFile;
+	parse_string();
 	return 0;
 }

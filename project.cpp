d506dd47969efed044e178f426dacbcffc0ada2d
Savio Concessio <savio.concessio@outlook.com>
2024-09-18 11:41:56 +1200

    Further development of state

--- project.cpp
+++ project.cpp
@@ -22,6 +22,9 @@ struct state {
 		return transitions.at(s);
 	}
 };
+enum parse_state{
+	R, A, S, I, T, It, D, N
+};
 
 vector<string> code;
 map<string, state>all_states;
@@ -29,9 +32,7 @@ stack<string> braces;//checks if braces are closed
 state editing;
 int i = 0;
 string input;
-enum parse_state{
-	R, A, S, I, T, It, D, N
-};
+string transition_key;
 parse_state cur_state = R;
 
 class ParseError : public std::exception {
@@ -67,17 +68,18 @@ void parse(string str){
                 to_add.accepting = true;  // Set the accepting state
                 all_states[str] = to_add;             
              }
-              if(input[i++] != '}') throw ParseError("Missing { @ R:");
+              if(input[i++] != '}') throw ParseError("Missing } @ A:");
               cur_state = R;
             break;
         }
         case S: {
 			 if(str.length() != 1) throw ParseError("Illegal amount of args @ R: " + str);
+			 editing = state();
 			 char c = str[0];
 			 if(c == 't') cur_state = S;
 			 else if(c == 'n') cur_state = A;
-             else throw ParseError("Bad char passed to case R: " + str);
-			 if(input[i++] != '{') throw ParseError("Missing { @ R:");
+             else throw ParseError("Bad char passed to case S: " + str);
+			 if(input[i++] != '{') throw ParseError("Missing { @ S:");
              break;
         }
         case I: {
@@ -89,13 +91,38 @@ void parse(string str){
                 to_add.init = true;
                 all_states[str] = to_add;             
             }
-            if(input[i++] != '}') throw ParseError("Missing { @ R:");
+            if(input[i++] != '}') throw ParseError("Missing } @ I:");
             cur_state = R;
             break;
         }
-        case T:
-        case It:
-        case D:
+        case T:{
+			if(str.length() != 1) throw ParseError("Illegal amount of args @ R: " + str);
+			char c = str[0];
+			if(c == 'i') cur_state = It;
+			else throw ParseError("Bad char passed to case S: " + str);
+			if(input[i++] != '{') throw ParseError("Missing { @ S:");
+		}
+        case It:{
+			transition_key = str;
+			if(input[i++] != '}') throw ParseError("Missing } @ It:");
+			if(input[i++] != 'd') throw ParseError("Missing d @ It:");
+			if(input[i++] != '{') throw ParseError("Missing { @ It:");
+			cur_state = D;
+			break;
+		}
+        case D: {
+			auto it = all_states.find(str);
+            if (it != all_states.end()) {
+				editing.add(transition_key, &(it->first));
+            } else {
+                editing.name = str;
+                all_states[str] = editing;   
+                
+            }
+            if(input[i++] != '{') throw ParseError("Missing { @ I:");
+            cur_state = R;
+			break;
+		}
         case N:
             break;
     }
